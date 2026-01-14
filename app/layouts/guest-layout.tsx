f7428ec775665f217cbec5d18d39a0bae1809350
duydev245 <duyhtse182314@fpt.edu.vn>
2026-01-14 15:57:47 +0700

    CODING: update color section menu && header

--- app/layouts/guest-layout.tsx
+++ app/layouts/guest-layout.tsx
@@ -1,12 +1,8 @@
 import { Footer } from '@/components/guest/Footer';
 import { Header } from '@/components/guest/Header';
-import useUserStore from '@/store/user.store';
 import { Outlet } from 'react-router';
 
 export default function GuestLayout() {
-  const user = useUserStore((s) => s.user);
-  console.log('🚀 ~ GuestLayout ~ user:', user);
-
   return (
     <div className='min-h-screen'>
       <Header />
