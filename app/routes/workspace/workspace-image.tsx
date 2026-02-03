4d9423d48ebfa3804c64d945132b619bed99a8fa
duydev245 <duyhtse182314@fpt.edu.vn>
2026-02-03 22:59:49 +0700

    CODING: sidebar config image generation for ws

--- app/routes/workspace/workspace-image.tsx
+++ app/routes/workspace/workspace-image.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import { WorkspaceImageSidebar } from '@/components/workspace/WorkspaceImageSidebar';
 
 export default function WorkspaceImage() {
-  return <div className='text-white'>WorkspaceImage</div>;
+  return (
+    <div className='flex h-[calc(100vh-4rem)]'>
+      <WorkspaceImageSidebar />
+      <main>workspace layout</main>
+    </div>
+  );
 }
