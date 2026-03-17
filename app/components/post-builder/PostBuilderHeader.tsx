a544384f34c879f0b3c5dc1aa9ea95568179fb10
duydev245 <duyhtse182314@fpt.edu.vn>
2026-03-17 14:20:13 +0700

    CODING: Init PostBuilder Header

--- app/components/post-builder/PostBuilderHeader.tsx
+++ app/components/post-builder/PostBuilderHeader.tsx
@@ -1,7 +1,15 @@
+import CoinIcon from '@/components/icons/CoinIcon';
+import type { TProfile } from '@/models/profile.model';
 import { ArrowLeftFromLineIcon } from 'lucide-react';
-import React from 'react';
+import { useNavigate } from 'react-router';
 
-function PostBuilderHeader() {
+interface TProps {
+  user?: TProfile | null;
+}
+
+function PostBuilderHeader({ user }: TProps) {
+  const navigate = useNavigate();
+
   return (
     <header className='h-16 sticky top-0 z-12 w-full bg-zinc-950 border-b border-zinc-900'>
       <div className='max-w-full mx-auto px-6 h-16 flex items-center justify-between'>
@@ -17,13 +25,13 @@ function PostBuilderHeader() {
           </div>
         </div>
 
-        {/* Center: Title & Tooltips*/}
-        <div className='flex-1 text-center'>
-          <h1 className='text-lg font-bold text-white'>Post Builder</h1>
-        </div>
-
         {/* Right: Actions (SaveDraftButton/ PublishButton) */}
         <div className='flex items-center justify-center gap-4'>
+          <div className='flex items-center justify-center gap-1 cursor-pointer px-4 py-2 rounded-md border border-purple-500 hover:bg-neutral-800/50'>
+            {/* icon coin */}
+            <CoinIcon />
+            <p className='text-md font-semibold text-white'>0</p>
+          </div>
           <button
             type='button'
             className='px-4 py-2 rounded-md border border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white transition-colors'
