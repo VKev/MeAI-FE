7ee3521bbf40dde8f98be0306a5bdcba25e80ca7
duydev245 <duyhtse182314@fpt.edu.vn>
2026-04-01 21:39:32 +0700

    CODING: Change to toast (react-toastify)

--- app/components/auth/SigninForm.tsx
+++ app/components/auth/SigninForm.tsx
@@ -3,12 +3,12 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Link, useFetcher, useNavigate, useSearchParams } from 'react-router';
 import { Eye, EyeOff } from 'lucide-react';
-import { toast } from 'sonner';
 import { useQueryClient } from '@tanstack/react-query';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
 import { SigninSchema, type TSigninValues } from '@/models/auth.model';
+import { toast } from 'react-toastify';
 
 type Props = {
   isActive: boolean;
