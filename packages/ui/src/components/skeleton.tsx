ea22d3266de8ae30381fa7310dc7ef067cfb683a
duydev245 <duyhtse182314@fpt.edu.vn>
2026-05-18 16:05:41 +0700

    merge editor

---
+++ packages/ui/src/components/skeleton.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@meai-editor/ui/lib/utils';
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />;
+}
+
+export { Skeleton };
